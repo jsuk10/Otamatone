d5c600d4593417cd7d69cb062c934b95496c44b6
jsuk10 <jsuk970407@gmail.com>
2021-07-07 15:07:35 +0900

    feat : Add Mulity touch

--- app/src/main/java/com/example/otamatone/MainActivity.java
+++ app/src/main/java/com/example/otamatone/MainActivity.java
@@ -4,8 +4,11 @@ import android.media.AudioManager;
 import android.media.SoundPool;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.Window;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -18,8 +21,12 @@ import com.google.android.gms.ads.initialization.OnInitializationCompleteListene
 
 public class MainActivity extends AppCompatActivity {
     private SoundPool pool;
+    private ImageView background;
+    private ImageButton[] button = new ImageButton[10];
     private AdView mAdView;
     int arr[] = new int[10];
+    int sound[] = new int[10];
+    int touchCount = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +36,8 @@ public class MainActivity extends AppCompatActivity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
         mAdView = findViewById(R.id.adView);
+
+        getSupportActionBar().hide();
         mAdView.setAdListener(new AdListener() {
             @Override
             public void onAdLoaded() {
@@ -50,19 +59,137 @@ public class MainActivity extends AppCompatActivity {
             public void onInitializationComplete(InitializationStatus initializationStatus) {
             }
         });
-
-        //        if (BuildConfig.DEBUG)
-//            mAdView.setAdUnitId("@string/banner_ad_unit_id_for_test");
-//        else
-//            mAdView.setAdUnitId("@string/admob_app_id");
         AdRequest adRequest = new AdRequest.Builder().build();
         mAdView.loadAd(adRequest);
-
+        SetImage();
         addMusic();
     }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        pool.release();
+        pool = null;
+    }
 
+    public void SetImage(){
+        for(int i = 0 ; i < 10 ; i++) {
+            int resId = getResources().getIdentifier("button" + (i + 1), "id", getPackageName());
+            button[i] = (ImageButton) findViewById(resId);
+            button[i].setImageResource(R.drawable.defultbutton);
+            button[i].setOnTouchListener(new View.OnTouchListener(){
+                public boolean onTouch(View v, MotionEvent event) {
+                    int id = v.getId();
+                    int status = event.getAction();
+                    if(status == MotionEvent.ACTION_UP) {
+                        actionUpEvent(id);
+                    }else if(status == MotionEvent.ACTION_DOWN) {
+                        actionDownEvent(id);
+                    }
+                    return false;
+                }
+            });
+        }
+        background = findViewById(R.id.backGround);
+        background.setImageResource(R.drawable.defultbackground);
+    }
+
+    public void actionUpEvent(int id){
+        touchCount--;
+        if(touchCount == 0)
+            background.setImageResource(R.drawable.defultbackground);
+        Log.e("tt",Integer.toString(id));
+        switch (id) {
+            case R.id.button10:
+                button[9].setImageResource(R.drawable.defultbutton);
+                pool.stop(sound[0]);
+                break;
+            case R.id.button9:
+                button[8].setImageResource(R.drawable.defultbutton);
+                pool.stop(sound[1]);
+                break;
+            case R.id.button8:
+                button[7].setImageResource(R.drawable.defultbutton);
+                pool.stop(sound[2]);
+                break;
+            case R.id.button7:
+                button[6].setImageResource(R.drawable.defultbutton);
+                pool.stop(sound[3]);
+                break;
+            case R.id.button6:
+                button[5].setImageResource(R.drawable.defultbutton);
+                pool.stop(sound[4]);
+                break;
+            case R.id.button5:
+                button[4].setImageResource(R.drawable.defultbutton);
+                pool.stop(sound[5]);
+                break;
+            case R.id.button4:
+                button[3].setImageResource(R.drawable.defultbutton);
+                pool.stop(sound[6]);
+                break;
+            case R.id.button3:
+                button[2].setImageResource(R.drawable.defultbutton);
+                pool.stop(sound[7]);
+                break;
+            case R.id.button2:
+                button[1].setImageResource(R.drawable.defultbutton);
+                pool.stop(sound[8]);
+                break;
+            case R.id.button1:
+                button[0].setImageResource(R.drawable.defultbutton);
+                pool.stop(sound[9]);
+                break;
+        }
+    }
+
+    public void actionDownEvent(int id){
+        touchCount++;
+        background.setImageResource(R.drawable.onclickbackground);
+        switch (id) {
+            case R.id.button10:
+                button[9].setImageResource(R.drawable.onclickbutton);
+                sound[0] = pool.play(arr[0], 1, 1, 0, -1, 1);
+                break;
+            case R.id.button9:
+                button[8].setImageResource(R.drawable.onclickbutton);
+                sound[1] = pool.play(arr[1], 1, 1, 0, -1, 1);
+                break;
+            case R.id.button8:
+                button[7].setImageResource(R.drawable.onclickbutton);
+                sound[2] = pool.play(arr[2], 1, 1, 0, -1, 1);
+                break;
+            case R.id.button7:
+                button[6].setImageResource(R.drawable.onclickbutton);
+                sound[3] = pool.play(arr[3], 1, 1, 0, -1, 1);
+                break;
+            case R.id.button6:
+                button[5].setImageResource(R.drawable.onclickbutton);
+                sound[4] = pool.play(arr[4], 1, 1, 0, -1, 1);
+                break;
+            case R.id.button5:
+                button[4].setImageResource(R.drawable.onclickbutton);
+                sound[5] = pool.play(arr[5], 1, 1, 0, -1, 1);
+                break;
+            case R.id.button4:
+                button[3].setImageResource(R.drawable.onclickbutton);
+                sound[6] = pool.play(arr[6], 1, 1, 0, -1, 1);
+                break;
+            case R.id.button3:
+                button[2].setImageResource(R.drawable.onclickbutton);
+                sound[7] = pool.play(arr[7], 1, 1, 0, -1, 1);
+                break;
+            case R.id.button2:
+                button[1].setImageResource(R.drawable.onclickbutton);
+                sound[8] = pool.play(arr[8], 1, 1, 0, -1, 1);
+                break;
+            case R.id.button1:
+                button[0].setImageResource(R.drawable.onclickbutton);
+                sound[9] = pool.play(arr[9], 1, 1, 0, -1, 1);
+                break;
+        }
+    }
     public void addMusic() {
-        pool = new SoundPool(1, AudioManager.STREAM_MUSIC, 0);
+        pool = new SoundPool(3, AudioManager.STREAM_MUSIC, 0);
         arr[0] = pool.load(this, R.raw.a, 1);
         arr[1] = pool.load(this, R.raw.b, 1);
         arr[2] = pool.load(this, R.raw.c, 1);
@@ -75,39 +202,4 @@ public class MainActivity extends AppCompatActivity {
         arr[9] = pool.load(this, R.raw.j, 1);
     }
 
-    public void mOnClick(View v) {
-
-        switch (v.getId()) {
-            case R.id.button10:
-                pool.play(arr[0], 1, 1, 0, 0, 1);
-                break;
-            case R.id.button9:
-                pool.play(arr[1], 1, 1, 0, 0, 1);
-                break;
-            case R.id.button8:
-                pool.play(arr[2], 1, 1, 0, 0, 1);
-                break;
-            case R.id.button7:
-                pool.play(arr[3], 1, 1, 0, 0, 1);
-                break;
-            case R.id.button6:
-                pool.play(arr[4], 1, 1, 0, 0, 1);
-                break;
-            case R.id.button5:
-                pool.play(arr[5], 1, 1, 0, 0, 1);
-                break;
-            case R.id.button4:
-                pool.play(arr[6], 1, 1, 0, 0, 1);
-                break;
-            case R.id.button3:
-                pool.play(arr[7], 1, 1, 0, 0, 1);
-                break;
-            case R.id.button2:
-                pool.play(arr[8], 1, 1, 0, 0, 1);
-                break;
-            case R.id.button1:
-                pool.play(arr[9], 1, 1, 0, 0, 1);
-                break;
-        }
-    }
 }
